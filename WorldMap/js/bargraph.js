9241a5fe81e0ae5fa8809711c5598dba2b6651ab
yusuzuki-ucb <yusuzuki@ischool.berkeley.edu>
2015-05-07 11:42:53 -0700

    Implemented animation (2)

--- WorldMap/js/bargraph.js
+++ WorldMap/js/bargraph.js
@@ -1,14 +1,25 @@
 var barWidth = document.getElementById('bargraph').offsetWidth - document.getElementById('label-us').offsetWidth;
 var barHeight = 30;
+//
+var svgUS, barUS;
+var svgOther, barOther;
 
-var svgUS = d3.select("#bar-us").append("svg").attr("width", barWidth).attr("height", barHeight);
-var barUS = svgUS.append("rect")//
-.attr("width", barWidth).attr("height", barHeight)//
-.attr("style", "fill: #790647;");
+// Initializing the bar for US
+function initBarUS() {
+    svgUS = d3.select("#bar-us").append("svg").attr("width", barWidth).attr("height", barHeight);
+    barUS = svgUS.append("rect")//
+    .attr("width", 0).attr("height", barHeight)//
+    .attr("style", "fill: #790647;")//
+    .transition().attr("width", barWidth)//
+    .duration(700).ease();
+}
 
-var svgOther = d3.select("#bar-other").append("svg").attr("width", barWidth).attr("height", barHeight);
-var barOther = svgOther.append("rect")//
-.attr("width", 0).attr("height", barHeight);
+// Initializing the bar for other country
+function initBarOther() {
+    svgOther = d3.select("#bar-other").append("svg").attr("width", barWidth).attr("height", barHeight);
+    barOther = svgOther.append("rect")//
+    .attr("width", 0).attr("height", barHeight);
+}
 
 function changeBar(d) {
     var name = d.properties.name;
@@ -17,8 +28,10 @@ function changeBar(d) {
     var w = barWidth * (value / tuitionsMax);
     
     d3.select("#label-other p").text(name);
-    barOther.attr("style", "fill: " + color + ";");
-    barOther.attr("width", w);
+    if (value > 0) {
+        barOther.attr("style", "fill: " + color + ";");
+    }
+    barOther.transition().attr("width", w).duration(400);
 }
 
 console.log();
