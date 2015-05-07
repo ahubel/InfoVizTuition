8b9fd1c5b67db987378c65525f7645f2e9246a37
yusuzuki-ucb <yusuzuki@ischool.berkeley.edu>
2015-05-07 13:59:06 -0700

    Added text to show the values of bar graph

--- WorldMap/js/bargraph.js
+++ WorldMap/js/bargraph.js
@@ -1,4 +1,4 @@
-var barWidth = document.getElementById('bargraph').offsetWidth - document.getElementById('label-us').offsetWidth;
+var barWidth = (document.getElementById('bargraph').offsetWidth - document.getElementById('label-us').offsetWidth) * 0.9;
 var barHeight = 30;
 //
 var svgUS, barUS;
@@ -6,17 +6,28 @@ var svgOther, barOther;
 
 // Initializing the bar for US
 function initBarUS() {
-    svgUS = d3.select("#bar-us").append("svg").attr("width", barWidth).attr("height", barHeight);
+    svgUS = d3.select("#bar-us").append("svg")//
+    .attr("width", barWidth).attr("height", barHeight)//
+    .attr("id", "svg-us");
+    
     barUS = svgUS.append("rect")//
     .attr("width", 0).attr("height", barHeight)//
     .attr("style", "fill: #790647;")//
     .transition().attr("width", barWidth)//
     .duration(700).ease();
+    
+    // Add text
+    var pos = document.getElementById("svg-us").offsetLeft + barWidth + 20;
+    d3.select("#value-us").attr("style", "left: " + pos + "px;");
+    d3.select("#value-us p").text("$" + tuitionsMax);
 }
 
 // Initializing the bar for other country
 function initBarOther() {
-    svgOther = d3.select("#bar-other").append("svg").attr("width", barWidth).attr("height", barHeight);
+    svgOther = d3.select("#bar-other").append("svg")//
+    .attr("id", "svg-other")//
+    .attr("width", barWidth).attr("height", barHeight);
+    
     barOther = svgOther.append("rect")//
     .attr("width", 0).attr("height", barHeight);
 }
@@ -27,11 +38,17 @@ function changeBar(d) {
     var color = d.properties.color;
     var w = barWidth * (value / tuitionsMax);
     
+    // Change bar
     d3.select("#label-other p").text(name);
     if (value > 0) {
         barOther.attr("style", "fill: " + color + ";");
     }
     barOther.transition().attr("width", w).duration(400);
+    
+    // Move text
+    var pos = document.getElementById("svg-other").offsetLeft + w + 20;
+    d3.select("#value-other p").text("$" + value);
+    d3.select("#value-other").transition()//
+    .attr("style", "left: " + pos + "px;")//
+    .duration(200);
 }
-
-console.log();
